d862f1a415de6ed312fed1ecc7470210274f0121
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2025-03-28 11:30:48 -0400

    Read file contents

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def translate_english(event):
     english = input_text.value
 
     # Run a Test Print Statement
+    # -> Prints don't display on HTML page
     print('* This is a test print statement')
     print('* Input:', english)
 
-    # Send pirate version back to the HTML file
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+    # Try to open file
+    with open(english, 'r') as input_file:
+        # Read in contents of file
+        # Send pirate version back to the HTML file
+        output_div = document.querySelector("#output")
+        output_div.innerText = input_file.read()
