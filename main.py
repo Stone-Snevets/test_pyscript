b4e27c7cd93cd35a3ddf461e570a98a100e4479e
Stone-Snevets <142818150+Stone-Snevets@users.noreply.github.com>
2025-04-04 10:04:51 -0400

    Add built-in file browsing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arrr
 from pyscript import document
+from tkinter import filedialog
 
 
 def translate_english(event):
@@ -7,14 +8,21 @@ def translate_english(event):
     input_text = document.querySelector("#english")
     english = input_text.value
 
-    # Run a Test Print Statement
-    # -> Prints don't display on HTML page
-    print('* This is a test print statement')
-    print('* Input:', english)
+   user_input = filedialog.askopenfile(initialdir='/',
+                                       title='Select a Question File to Open',
+                                       filetypes=(('Text File', '*.txt'),
+                                                  ('PDF File', '*.pdf'),
+                                                  ('DOCX File', '*.docx')))
 
+    # Send the file path to the output
+    output_div = document.querySelector('#output')
+    output_div.innerText = user_input
+
+    """
     # Try to open file
-    with open(english, 'r') as input_file:
+    with open(user_input, 'r') as input_file:
         # Read in contents of file
         # Send pirate version back to the HTML file
         output_div = document.querySelector("#output")
         output_div.innerText = input_file.read()
+    """
